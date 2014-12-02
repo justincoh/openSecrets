08397bef67e49ad6b3b06f7013ef9187e4205991
justincoh <justincoh@gmail.com>
2014-12-02 12:42:17 -0500

    Pie Slice Toggling working

--- pieCharts.js
+++ pieCharts.js
@@ -11,6 +11,11 @@ var arc = d3.svg.arc()
     .outerRadius(radius - 10)
     .innerRadius(radius*.4);
 
+var transitionArc = d3.svg.arc()
+      .outerRadius(radius)
+      .innerRadius(radius*.5);
+
+
 var pie = d3.layout.pie()
     .sort(null)
     .value(function(d) { return d.total; });
@@ -51,12 +56,13 @@ function createPieChart(data){
       .style("text-anchor", "middle")
       .text(function(d) { return d.data.industry; });
 
-  //For tooltip positioning
-
+  //For tooltip positioning, though you might want to remove tooltip entirely
+  //and just have each click print things on the side in a list
+  //but each path is going to need to have its associated data
     var element = d3.selectAll('svg')
     element = element[0][0];
     var bbox = element.getBBox();
-   var tooltip = d3.select('body').append('div')
+    var tooltip = d3.select('body').append('div')
               .style('position', 'absolute')
               .style('padding', '0 10px')
               .style('background', 'black')
@@ -64,17 +70,17 @@ function createPieChart(data){
               .style('opacity', 0) // setting to 0 because we dont want it to show when the graphic first loads
               .style('font-size','12px')
           d3.selectAll('path').on('click', function(d) {
-            // console.log('D ',d)
-            d3.select(this)
-              .style('opacity', 0.5)
+            pieSliceToggle(this);
             tooltip.transition()
               .style('opacity', .9)
-            tooltip.html(((d['value']/sum)*100).toFixed(2)+'% of total funding')
+            tooltip.html((
+              (d['value']/sum)*100)
+                .toFixed(2)+'% of total funding')
               // console.log(d)
               // .style('left', (d3.event.pageX -15) + 'px')
               // .style('top', (d3.event.pageY - 30) + 'px')
-              .style('left', (bbox.width) + 'px')
-              .style('top', (bbox.height*.7) + 'px')
+              .style('left', (bbox.width*.5) + 'px')
+              .style('top', (bbox.height*.5) + 'px')
                 
           })
             .on('mouseout', function(d) {
@@ -83,9 +89,21 @@ function createPieChart(data){
                 tooltip.transition()
                   .style('opacity', 0)
             })
-
-
-// });
 }
+
+////Pie Chart Transitions
+////Pop in and out on click
+function pieSliceToggle(element){
+  var thisPath = d3.select(element);
+    if(thisPath.attr('transitioned')==='true'){
+      thisPath.transition().attr('d', arc)
+      thisPath.attr('transitioned', false)
+    } else {
+      thisPath.transition().attr('d', transitionArc)
+      thisPath.attr('transitioned', true)
+    }
+};
+
+
 
 // module.exports = createPieChart;
