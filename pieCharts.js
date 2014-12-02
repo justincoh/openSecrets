c2a04157d416bf210e5bf82135c6ace2b7ccbcea
justincoh <justincoh@gmail.com>
2014-12-01 19:17:40 -0500

    Tooltip with %s working

--- pieCharts.js
+++ pieCharts.js
@@ -17,20 +17,20 @@ var pie = d3.layout.pie()
 
 
 function createPieChart(data){
-var svg = d3.select("body").append("svg")
+
+  var sum=0; //for calculating percentages below
+  for(var i = 0; i<data.length;i++){
+    sum += data[i]['total'];
+  //Or should I store this as a key of the obj?
+  }
+
+
+  var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height)
   .append("g")
     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
     // console.log("SVG: ",Object.keys(svg[0][0]).sort())
-
-// var data = [   //This was test data
-// 	{age:'<5',population:270},
-// 	{age:'5-13',population:449},
-// 	{age:'14-17',population:215},
-// 	{age:'Junk',population:300},
-// 	{age:'More Junk',population:50}
-// 	]
 
   var g = svg.selectAll(".arc")
       .data(pie(data))
@@ -50,6 +50,33 @@ var svg = d3.select("body").append("svg")
       .attr("dy", ".35em")
       .style("text-anchor", "middle")
       .text(function(d) { return d.data.industry; });
+
+
+  var tooltip = d3.select('body').append('div')
+          .style('position', 'absolute')
+          .style('padding', '0 10px')
+          .style('background', 'black')
+          .style('color','white')
+          .style('opacity', 0) // setting to 0 because we dont want it to show when the graphic first loads
+
+      d3.selectAll('path').on('mouseover', function(d) {
+        // console.log('D ',d)
+        d3.select(this)
+          .style('opacity', 0.5)
+        tooltip.transition()
+          .style('opacity', .9)
+        tooltip.html(((d['value']/sum)*100).toFixed(2)+'%')
+          // console.log(d)
+          .style('left', (d3.event.pageX -15) + 'px')
+          .style('top', (d3.event.pageY - 30) + 'px')
+      })
+        .on('mouseout', function(d) {
+            d3.select(this)
+              .style('opacity', 1)
+            tooltip.transition()
+              .style('opacity', 0)
+        })
+
 
 // });
 }
