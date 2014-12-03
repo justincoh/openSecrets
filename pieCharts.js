e6479cdc482dd11d29634fa9ade89e5da0a05714
justincoh <justincoh@gmail.com>
2014-12-03 11:38:40 -0500

    Issues with git

--- pieCharts.js
+++ pieCharts.js
@@ -1,6 +1,6 @@
 // var express = require('express');
 
-var width = 960,
+var width = 600,
     height = 500,
     radius = Math.min(width, height) / 2;
 
@@ -24,14 +24,14 @@ var sum=0; //Will be used throughout this file
 
 function createPieChart(data){
 
-  
+
   for(var i = 0; i<data.length;i++){
     sum += data[i]['total'];
   //Or should I store this as a key of the obj?
   }
 
 
-  var svg = d3.select("body").append("svg")
+  var svg = d3.select("#repPieChart").append("svg")
     .attr("width", width)
     .attr("height", height)
     .append("g")
@@ -47,7 +47,7 @@ function createPieChart(data){
   g.append("path")
       .attr("d", arc)
       .attr("funding",function(d){return d.data.total})
-      .style("fill", function(d) { 
+      .style("fill", function(d) {
       	// console.log(d.data.age,": Start Angle , ",d.startAngle)  //Getting angles for each arc (in radians)
       	// console.log(d.data.age,": End Angle , ",d.endAngle)  //Getting angles for each arc
       	return color(d.data.industry); });
@@ -85,7 +85,7 @@ function createPieChart(data){
               // .style('top', (d3.event.pageY - 30) + 'px')
               // .style('left', (bbox.width*.5) + 'px')
               // .style('top', (bbox.height*.5) + 'px')
-                
+
           })
             .on('mouseout', function(d) {
                 d3.select(this)
@@ -115,7 +115,7 @@ function formatMoney (num) {
       .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
 }
 
-////JQuery function that is called on every click to populate financial summary 
+////JQuery function that is called on every click to populate financial summary
 ////on the side of the page
 ////am going to have to refactor the HTML to make this layout work
 function populateSummary(){
@@ -136,12 +136,12 @@ function populateSummary(){
   var totalLi=$('<li/>')
     .text(formatMoney(activeTotal));
 
-  
+
   console.log($percentage)
   $summary.text(industries.join(', '))
   totalLi.appendTo($summary);
   percentLi.appendTo($summary);
-  
+
 }
 
 
