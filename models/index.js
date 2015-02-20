ed3f548cdf002ef68058873a5881c153f1e585b5
justincoh <justincoh@gmail.com>
2015-02-20 12:11:54 -0500

    mongolab hooked up

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/openSecrets');
+// mongoose.connect('mongodb://localhost/openSecrets');
+mongoose.connect(process.env.MONGOLAB_URI)
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
 var findOrCreate = require('mongoose-findorcreate');
